f5a607a40c6c3011c33f4b6dff1c629361e831a9
BrayhanJaramillo <brayhanjaramillo@gmail.com>
2017-12-06 13:29:54 -0500

    Version 10.0

--- __openerp__.py
+++ __openerp__.py
@@ -11,9 +11,14 @@
                'Contact Module - Odoo 9.0',
     'images': ['images/main_screenshot.png'],
     'depends': [
+        'account',
+        'account_accountant',
+        'base'
     ],
     'data': [
         'views/l10n_co_res_partner.xml',
+        'views/ciiu.xml',
+        'data/ciiu.csv',
         'data/l10n_states_co_data.xml',
         'data/l10n_cities_co_data.xml',
         'security/ir.model.access.csv'
